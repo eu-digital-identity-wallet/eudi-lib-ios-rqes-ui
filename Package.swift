aa5591bab35569ff280498ead99c5c1af58f734e
Dimitris Tsiflitzis <129499163+dtsiflit@users.noreply.github.com>
2026-02-12 12:00:45 +0200

    rqes kit update

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-rqes-kit.git",
-      exact: "0.6.4"
+      exact: "0.7.0"
     ),
     .package(
       url: "https://github.com/Brightify/Cuckoo.git",
